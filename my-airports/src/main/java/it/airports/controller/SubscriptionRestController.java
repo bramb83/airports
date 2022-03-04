3dab69f7297c89acd5f57341ee13102f806fa8a7
bramb83 <56624865+bramb83@users.noreply.github.com>
2022-03-04 16:37:50 +0100

    Update SubscriptionRestController.java

--- my-airports/src/main/java/it/airports/controller/SubscriptionRestController.java
+++ my-airports/src/main/java/it/airports/controller/SubscriptionRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import it.airports.model.Subscriptions;
 import it.airports.repository.SubscriptionsRepository;
 
-import it.airports.model.Metar;
-import it.airports.repository.MetarRepository;
 
 @RestController
 @RequestMapping("/subscriptions")
@@ -23,8 +21,6 @@ public class SubscriptionRestController {
 	@Autowired
 	SubscriptionsRepository subsService;
 	
-	@Autowired
-	MetarRepository metarService;
 	
 	@GetMapping("/airports")
 	 public Iterable<Subscriptions> getAirports() {
